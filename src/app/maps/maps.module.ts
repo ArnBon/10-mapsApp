1a898fe6ca03cf95a18afad21ec905c0e418163c
ArnBon <bonillaarnaldo@gmail.com>
2023-12-27 19:14:51 -0400

    clase 286 crear los componentes y rutas

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MapsRoutingModule } from './maps-routing.module';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+
+@NgModule({
+  declarations: [
+    MiniMapComponent,
+    MapsLayoutComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent
+  ],
+  imports: [
+    CommonModule,
+    MapsRoutingModule
+  ]
+})
+export class MapsModule { }
