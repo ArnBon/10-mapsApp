6b9a7ba4992dd9656dbb41e3dedcecb9682d7421
ArnBon <bonillaarnaldo@gmail.com>
2023-12-30 10:36:35 -0400

    clase 296 anadir marcadores de forma dinamica

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Map, LngLat, Marker } from 'mapbox-gl';
-
 @Component({
   selector: 'app-markers-page',
   templateUrl: './markers-page.component.html',
@@ -37,7 +36,25 @@ export class MarkersPageComponent {
 
   }
 
+  addMarker( lngLat: LngLat, color: string ) {
+    if ( !this.map ) return;
 
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat( lngLat )
+      .addTo( this.map );
+  }
+
+  createMarker() {
+    if ( !this.map ) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+
+    this.addMarker( lngLat, color );
+  }
 
 
 
