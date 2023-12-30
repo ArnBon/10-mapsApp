739d702c36e9e0c8f01508477506794ca1b215fa
ArnBon <bonillaarnaldo@gmail.com>
2023-12-29 20:44:39 -0400

    clase 292 diseno zoomIn zoomOut

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range-page',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class ZoomRangePageComponent {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40],
+      zoom: 9, // starting zoom
+    });
+  }
 }
