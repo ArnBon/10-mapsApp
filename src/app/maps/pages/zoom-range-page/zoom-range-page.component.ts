c428ec869dfeae537cdb893fdfd46c9630f74019
ArnBon <bonillaarnaldo@gmail.com>
2023-12-29 21:01:09 -0400

    clase 293 completada

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -10,16 +10,58 @@ export class ZoomRangePageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.10380784179445, 4.651165392795477);
+
 
   ngAfterViewInit(): void {
 
     if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40],
-      zoom: 9, // starting zoom
+      center: this.currentLngLat,
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
   }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  mapListeners() {
+    if ( !this.map ) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if ( this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+    });
+
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value: string ) {
+    this.zoom = Number(value);
+    this.map?.zoomTo( this.zoom );
+  }
+
 }
