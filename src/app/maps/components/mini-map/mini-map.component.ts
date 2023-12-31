c62d478e6a0f3c00a930818b85843c5890306864
ArnBon <bonillaarnaldo@gmail.com>
2023-12-31 09:17:42 -0400

    clase 302 componente minimapa

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
 export class MiniMapComponent {
 
+  @Input()lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(){
+    if(!this.divMap?.nativeElement) throw "Mapa no encontrado";
+    if(!this.lngLat) throw "La longitud y Latitud no puede estar vacia";
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new Marker()
+    .setLngLat(this.lngLat)
+    .addTo(map)
+  }
 }
